fe378df77c4c20207ba779a169a6f7e60dfecb4a
HarryCandlish <hcnd90@gmail.com>
2019-08-17 12:52:50 +1200

    home-page design completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,112 +2,91 @@ import React, { Component } from "react";
 
 import homeStyles from "../modules/home.module.scss";
 
-import projects from "../images/projects.png";
-import team from "../images/team.png";
-import practice from "../images/practice.png";
-
-import business from "../images/business.jpg";
 import people from "../images/people.jpg";
-import info from "../images/info.png";
+
+import website from "../images/website.JPG";
+
+import practice from "../images/practice.png";
+import projects from "../images/projects.png";
 
 class Home extends Component {
   render() {
     return (
       <div>
         <div className={homeStyles.titleContainer}>
-          <h1 className={homeStyles.title}>PEARL COLLECTIVE.</h1>
+          <h1 className={homeStyles.title}>Pearl Collective.</h1>
         </div>
         <div className={homeStyles.gridContainer}>
-          <div className={homeStyles.itemOne}>
-            <img className={homeStyles.workImage} alt="alt" src={business} />
-          </div>
           <div className={homeStyles.overlay}>
             <h1 className={homeStyles.gridTitleOne}>
-              HOW WE CAN HELP YOUR BUSINESS.
+              How we can help your business.
             </h1>
             <p className={homeStyles.gridParagraphOne}>
-              If you own, manage a small-business, non-profit, or social
-              enterprise, we'll set you up with junior developers and designers
-              willing to volunteer their services and skills.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
             </p>
             <button className={homeStyles.gridButtonOne}>SIGN UP</button>
           </div>
-          <div className={homeStyles.gridContainerTwo}>
-            <div className={homeStyles.itemTwo}>
-              <img className={homeStyles.teamImage} alt="alt" src={people} />
-            </div>
-            <div className={homeStyles.overlayTwo}>
-              <h1 className={homeStyles.gridTitleTwo}>
-                HOW WE CAN HELP YOU FIND EMPLOYMENT.
-              </h1>
-              <p className={homeStyles.gridParagraphTwo}>
-                Our goal is to simulate experience to better prepare juniors for
-                finding employment. You'll meet with clients, design, promote
-                web presence and develop websites for companies in Wellington.
-              </p>
-              <button className={homeStyles.gridButtonTwo}>JOIN US</button>
-            </div>
+          <div>
+            <img className={homeStyles.image} src={website} alt="website" />
           </div>
         </div>
-        <div>
-          <div className={homeStyles.containerFour}>
-            <div className={homeStyles.infoContainer}>
-              <div className={homeStyles.infoContainerTitle}>
-                <h1 className={homeStyles.infoTitle}>PEARL COLLECTIVE.</h1>
-              </div>
-              <p className={homeStyles.info}>
-                Our service works to provide small, non-profit businesses, and
-                social enterprises with websites. We write content, design, meet
-                with clients and develop the product. All developers, designers,
-                project managers, are juniors seeking experience to find
-                employment, and we work to simulate the experience of working
-                for a company.
-              </p>
-              <img className={homeStyles.infoImage} alt="info" src={info} />
-            </div>
+        <div className={homeStyles.gridContainerTwo}>
+          <div className={homeStyles.overlayTwo}>
+            <h1 className={homeStyles.gridTitleTwo}>
+              Are you a junior seeking employment?
+            </h1>
+            <p className={homeStyles.gridParagraphTwo}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <button className={homeStyles.gridButtonTwo}>JOIN US</button>
           </div>
-        </div>
-        <ul className={homeStyles.container}>
-          <li className={homeStyles.flexbox}>
-            <img
-              className={homeStyles.imageProjects}
-              alt="projects"
-              src={projects}
-            />
 
-            <button className={homeStyles.buttonOne}>PROJECTS</button>
-            <p className={homeStyles.paragraph}>
+          <div>
+            <img className={homeStyles.imageTwo} src={people} alt="people" />
+          </div>
+        </div>
+        <div className={homeStyles.flexContainer}>
+          <div className={homeStyles.flexbox}>
+            <h1 className={homeStyles.flexTitle}>
+              Helping businesses get ahead.
+            </h1>
+            <p className={homeStyles.flexParagraph}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
             </p>
-          </li>
-          <li className={homeStyles.flexbox}>
-            <img className={homeStyles.imageTeam} alt="team" src={team} />
-            <button className={homeStyles.buttonTwo}>TEAM</button>
-            <p className={homeStyles.paragraph}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </li>
-          <li className={homeStyles.flexbox}>
             <img
-              className={homeStyles.imagePractice}
-              alt="practice"
-              src={practice}
+              className={homeStyles.projects}
+              src={projects}
+              alt="projects"
             />
-            <button className={homeStyles.buttonThree}>PRACTICE</button>
-            <p className={homeStyles.paragraph}>
+            <button className={homeStyles.flexButtonOne}>PROJECTS</button>
+          </div>
+          <div className={homeStyles.flexbox}>
+            <h1 className={homeStyles.flexTitle}>
+              We simulate experience for juniors.
+            </h1>
+            <p className={homeStyles.flexParagraph}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
             </p>
-          </li>{" "}
-        </ul>
+            <img
+              className={homeStyles.practice}
+              src={practice}
+              alt="practice"
+            />
+            <button className={homeStyles.flexButtonTwo}>PRACTICE</button>
+          </div>
+        </div>
       </div>
     );
   }
